00aec7d210b5e0af24c489f1d41548a6d4f25595
Sridhar <sridharonstar@gmail.com>
2023-08-26 10:47:02 +0530

    flash button

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -4,12 +4,13 @@ import 'package:quiz_app/screens/flash_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
     home: Scaffold(
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(colors:[Color.fromRGBO(58, 169, 243, 1),Color.fromRGBO(168, 243, 121, 1)],
           begin: Alignment.topLeft,
-           )
+           ),
         ),
         child: const FlashScreen()),
     ),
--- quiz_app/lib/screens/flash_screen.dart
+++ quiz_app/lib/screens/flash_screen.dart
@@ -5,6 +5,18 @@ class FlashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child:  Text("Flash Screen"));
+    return  Center(child:Padding(
+      padding: const EdgeInsets.only(top: 50, left: 20, right: 20,bottom: 20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset("images/quiz-logo.png",width: 300,),
+          const SizedBox(height: 50,),
+          const Text("Flutter",style:TextStyle(fontSize: 50.0,fontWeight: FontWeight.w600,color: Colors.white),),
+          const SizedBox(height: 50,),
+          OutlinedButton.icon(onPressed: (){},style: OutlinedButton.styleFrom(foregroundColor: Colors.white),label: const Text("Start Quiz!"),icon: const Icon(Icons.arrow_right_alt),),
+        ],
+      ),
+    ));
   }
 }
